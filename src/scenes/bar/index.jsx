27e06611952dec524684957ea944da69de28cf51
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-01-03 17:32:18 +0200

    Done with Bar Chart

--- src/scenes/bar/index.jsx
+++ src/scenes/bar/index.jsx
@@ -1,3 +1,14 @@
+import { Box } from "@mui/material";
+import { BarChart } from "../../components/BarChart.jsx";
+import { Header } from "../../components/Header.jsx";
+
 export const Bar = () => {
-  return <div>Bar</div>;
+    return (
+        <Box m='20px'>
+            <Header title='BAR CHART' subtitle='Simple Bar Chart' />
+            <Box height='75vh'>
+                <BarChart isDashboard={true} />
+            </Box>
+        </Box>
+    );
 };

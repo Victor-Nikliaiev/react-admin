69d559d264f4f4f8658d325bcc6680f09f2ec7aa
Victor-Nikliaiev <victor.nikliaiev@gmail.com>
2023-01-04 12:00:53 +0200

    Done with Geography Component

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,3 +1,14 @@
+import { Box } from "@mui/material";
+import { Header } from "../../components/Header.jsx";
+import { LineChart } from "../../components/LineChart.jsx";
+
 export const Line = () => {
-  return <div>Line</div>;
+    return (
+        <Box m='20px'>
+            <Header title='LINE CHART' subtitle='Simple Line Chart' />
+            <Box height='75vh'>
+                <LineChart />
+            </Box>
+        </Box>
+    );
 };
